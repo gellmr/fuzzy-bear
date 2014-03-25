d754a0cf46114d059d889d7528d9289083710bca
Mike Gell <gellmr@gmail.com>
2014-03-26 00:18:28 +0800

    More styling.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -49,148 +49,127 @@ include_once getcwd() . '/application/libraries/util.php'; // not sure if this i
   </head>
 
   <body>
-    <h3>Fuzzy Bear Electronics</h3>
-    
-    <div id='disclaimer'>
-      This is not a real site.<br />I'm just demonstrating my amazing web application skills.
-    </div>
-    
-    <div id="login_panel_div">
-      
-      <div id="loginStatus" class="small_Text">
-
-        <?php
-        // Thin grey strip across the top of the login panel (Top right of page)
-        // CURRENTLY LOGGED IN AS BLAH
-        if (
-          ( isset($currentUser) && $currentUser != $NO_USER )
-          &&
-          ( isset($login_status) && $login_status == $LOGGED_IN )
-        )
-        {
-          // Logged in as blah.
-          echo "Currently logged in as $currentUser";
-        }
-        else
-        {
-          // NOT LOGGED IN.
-          echo "Not currently logged in.";
-        }
-        //echo " $page_title";
-        ?>
-      </div>
-      
-      
-      <div id="login_panel_all_buttons">
-        
-        <?php
-        
-        // TODO: remove all controller logic from templates.
-
-        function echo_anchor($controller, $text, $attribs, $current_page, $css_class, $divId='')
-        {
-          if ($current_page == $text)
-          {
-            // No anchor. Already on this page.
-            echo util::bright_PNG_anchor($text, $css_class, $divId);
-          }
-          else
-          {
-            // Yes anchor. We are not on this page.
-            echo util::dark_PNG_anchor($controller, $text, $attribs, $css_class, $divId);
-          }
-        }
-        
-        
-        
-        // Display all the items in the login panel (top right of screen)
-        //
-        if (isset($login_panel_contents))
-        {
-          foreach ($login_panel_contents as $index => $value)
-          {
-            
-            // -------------------------------------------------
-            // -------------------------------------------------
-            // -------------------------------------------------
-            // -------------------------------------------------
-            
-            if ($value == 'forgot_pw')
-            {
-              // Link - I forgot my password
-              // THIS IS DISPLAYED IF WE ARE LOGGED OUT.
-
-              $cssClass = "login_panel_button forgot_button btn btn-primary";
-              echo_anchor("forgotPassword", "I forgot my password", 'title="I forgot my password"', $page_title, $cssClass);
-            }
-            elseif ($value == 'register')
-            {
-              // Link - Register
-              // THIS IS DISPLAYED IF WE ARE LOGGED OUT.
-
-              $cssClass = "login_panel_button register_button btn btn-primary";
-              echo_anchor("register", "Register", 'title="Register"', $page_title, $cssClass);
-            }
-            elseif ($value == 'login')
-            {
-              // Link - Login
-              // THIS IS DISPLAYED IF WE ARE LOGGED OUT.
-
-              $cssClass = "login_panel_button login_button btn btn-primary";
-              echo echo_anchor("login", "Login", 'title="Login"', $page_title, $cssClass); // allow user to login
-              
-              // -------------------------------------------------
-              // -------------------------------------------------
-              // -------------------------------------------------
-              // -------------------------------------------------
-              
-            }
-            elseif ($value == 'my_account')
-            {
-              // Link - My Account
-              // ONLY DISPLAYED IF WE ARE LOGGED IN.
-
-              $div_id = "myAccount_button";
-              $cssClass = "login_panel_button";
-              echo echo_anchor("myAccount", "My Account", 'title="My Account"', $page_title, $cssClass, $div_id);
-            }
-            elseif ($value == 'logout')
-            {
-              // Link - Logout
-              // ONLY DISPLAYED IF WE ARE LOGGED IN.
-
-              $cssClass = "login_panel_button login_button";
-              echo echo_anchor("logout", "Logout", 'title="Logout"', $page_title, $cssClass); // allow user to logout
-              
-              // -------------------------------------------------
-              // -------------------------------------------------
-              // -------------------------------------------------
-              // -------------------------------------------------
-            }
-          }
-        }
-        
-        ?>
-          
-      </div>
-    </div>
-    
-    <?php if($showDebug && $globalShowDebug) { ?>
-      
-      <div id="debugPanel">
-        <?php
-        if(isset($debugText))
-        {
-          echo $debugText;
-        }
-        else
-        {
-          echo "debug panel";
-        }
-        ?>
-      </div>
-      
-    <?php } ?>
 
     <div class="container">
+
+      <div class="row">
+      
+        <div class="col-xs-8">
+          <div class="row">
+            <div class="col-xs-12">
+              <h3>Fuzzy Bear Electronics</h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12">
+              <div id='disclaimer'>
+                This is not a real site.<br />I'm just demonstrating my amazing web application skills.
+              </div>
+            </div>
+          </div>
+        </div> <!-- col 8 -->
+
+        <div class="col-xs-4">
+          <div class="row" id="loginStatus" class="small_Text">
+            <div class="col-xs-12">
+              <?php
+              // Thin grey strip across the top of the login panel (Top right of page)
+              // CURRENTLY LOGGED IN AS BLAH
+              if (
+                ( isset($currentUser) && $currentUser != $NO_USER )
+                &&
+                ( isset($login_status) && $login_status == $LOGGED_IN )
+              )
+              {
+                // Logged in as blah.
+                echo "Currently logged in as $currentUser";
+              }
+              else
+              {
+                // NOT LOGGED IN.
+                echo "Not currently logged in.";
+              }
+              //echo " $page_title";
+              ?>
+            </div> <!-- col 12 -->
+          </div> <!-- row -->
+          
+          <div class="row">
+            <div class="col-xs-12">
+              <?php
+              // TODO: remove all controller logic from templates.
+              function echo_anchor($controller, $text, $attribs, $current_page, $css_class, $divId=''){
+                if ($current_page == $text){
+                  // No anchor. Already on this page.
+                  echo util::bright_PNG_anchor($text, $css_class, $divId);
+                }else{
+                  // Yes anchor. We are not on this page.
+                  echo util::dark_PNG_anchor($controller, $text, $attribs, $css_class, $divId);
+                }
+              }
+              // Display all the items in the login panel (top right of screen)
+              //
+              if (isset($login_panel_contents)) {
+                foreach ($login_panel_contents as $index => $value) {
+                  // -------------------------------------------------
+                  if ($value == 'forgot_pw') {
+                    // Link - I forgot my password
+                    // THIS IS DISPLAYED IF WE ARE LOGGED OUT.
+                    $cssClass = "login_panel_button forgot_button btn btn-primary";
+                    echo_anchor("forgotPassword", "I forgot my password", 'title="I forgot my password"', $page_title, $cssClass);
+                  }
+                  elseif ($value == 'register')
+                  {
+                    // Link - Register
+                    // THIS IS DISPLAYED IF WE ARE LOGGED OUT.
+                    $cssClass = "login_panel_button register_button btn btn-primary";
+                    echo_anchor("register", "Register", 'title="Register"', $page_title, $cssClass);
+                  }
+                  elseif ($value == 'login')
+                  {
+                    // Link - Login
+                    // THIS IS DISPLAYED IF WE ARE LOGGED OUT.
+                    $cssClass = "login_panel_button login_button btn btn-primary";
+                    echo echo_anchor("login", "Login", 'title="Login"', $page_title, $cssClass); // allow user to login
+                    // -------------------------------------------------
+                  }
+                  elseif ($value == 'my_account')
+                  {
+                    // Link - My Account
+                    // ONLY DISPLAYED IF WE ARE LOGGED IN.
+                    $div_id = "myAccount_button";
+                    $cssClass = "login_panel_button";
+                    echo echo_anchor("myAccount", "My Account", 'title="My Account"', $page_title, $cssClass, $div_id);
+                  }
+                  elseif ($value == 'logout')
+                  {
+                    // Link - Logout
+                    // ONLY DISPLAYED IF WE ARE LOGGED IN.
+                    $cssClass = "login_panel_button login_button";
+                    echo echo_anchor("logout", "Logout", 'title="Logout"', $page_title, $cssClass); // allow user to logout
+                    // -------------------------------------------------
+                  }
+                }
+              }
+              ?>
+            </div> <!-- col 12 -->
+          </div> <!-- row -->
+        </div> <!-- col 4 -->
+        
+        <?php
+        if($showDebug && $globalShowDebug) {
+          ?>
+          <div id="debugPanel">
+            <?php
+            if(isset($debugText)) {
+              echo $debugText;
+            }else{
+              echo "debug panel";
+            }
+            ?>
+          </div>
+          <?php
+        } ?>
+      </div> <!-- row -->
+
       <!-- (END HTML_Fragment) View: Store (Header) -->
