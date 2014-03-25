c070621b24ab06ed02761f5c525c47970a0a038b
Mike Gell <gellmr@gmail.com>
2014-03-25 23:56:06 +0800

    More styling.

--- application/views/templates/ribbon.php
+++ application/views/templates/ribbon.php
@@ -3,65 +3,38 @@
 <!-- (HTML_Fragment) View: Store (Ribbon) -->
 
 <div class="row">
-	
-	<?php
-	
-	function echo_ribbon_link($controller, $title, $attribs, $current_pageTitle, $divclass='', $divid='')
-	{
-		// Put an anchor (unless we are already on the page.)
-		
-		if ($current_pageTitle == $title)
-		{
-			// No anchor. Already on the page.
-			
-			echo util::bright_PNG_anchor($title, $divclass, $divid);
-		}
-		else
-		{
-			// Yes anchor. Not already on the page. 
-			
-			echo util::dark_PNG_anchor($controller, $title, $attribs, $divclass, $divid);
-		}
-	}
-	
-	
-	if (!isset($page_title))
-	{
-		$page_title = "";
-	}
-	
-	?>
-	
-	<?php
-	$div_class = "ribbonLink";
-	$div_id = "storeLink";
-	// echo_ribbon_link("store", "Store", 'title="Online Store"', $page_title, $div_class, $div_id);
+  
+  <?php
+  if (!isset($page_title))
+  {
+    $page_title = "";
+  }
+  ?>
+  
+  <?php
+  $div_class = "ribbonLink";
+  $div_id = "storeLink";
   echo anchor("store", "Store", 'id="storeLink" class="ribbonLink btn btn-primary" title="Online Store"');
-	?>
-	
-	<?php
-	$div_class = "ribbonLink";
-	$div_id = "cartLink";
-	// echo_ribbon_link("cart", "My Cart", 'title="My Cart"', $page_title, $div_class, $div_id);
+  ?>
+  
+  <?php
+  $div_class = "ribbonLink";
+  $div_id = "cartLink";
   echo anchor("cart", "My Cart", 'id="cartLink" class="ribbonLink btn btn-primary" title="My Cart"');
-	?>
+  ?>
 
-	<?php
-	$div_class = "ribbonLink";
-	$div_id = "checkoutLink";
-	// echo_ribbon_link("checkout/shippingMethod", "Checkout", 'title="Checkout"', $page_title, $div_class, $div_id);
+  <?php
+  $div_class = "ribbonLink";
+  $div_id = "checkoutLink";
   echo anchor("checkout/shippingMethod", "Checkout", 'id="checkoutLink" class="ribbonLink btn btn-primary" title="Checkout"');
-	?>
+  ?>
 
-	<?php
-	$div_class = "ribbonLink";
-	$div_id = "myOrderstLink";
-	// echo_ribbon_link("notImplementedYet", "My Orders", 'title="My Orders"', $page_title, $div_class, $div_id);
+  <?php
+  $div_class = "ribbonLink";
+  $div_id = "myOrderstLink";
   echo anchor("notImplementedYet", "My Orders", 'id="myOrderstLink" class="ribbonLink btn btn-primary" title="My Orders"');
-	?>
-	
-
+  ?>
+  
 </div>
 
-		
 <!-- (END HTML_Fragment) View: Store (Ribbon) -->
